f9e45916d0752d09409bece27e25798bf983655d
Truc Pham <121183450+Truc4p@users.noreply.github.com>
2024-07-12 12:02:38 +0700

    Update server.py

--- exc/server.py
+++ exc/server.py
@@ -23,9 +23,9 @@ def query():
     # Enhanced language detection logic
     def is_vietnamese(question):
         # Vietnamese-specific characters
-        vietnamese_chars = "ủũụọõỏẹẻẽịĩỉìạãảàăâđêôơư"
+        vietnamese_chars = "ủũụọõỏẹẻẽịĩỉìạãảàăâđêôơưđ"
         # Common Vietnamese words or phrases without accents
-        vietnamese_keywords = ['la', 'ai', 'ue', 'oi', 'au', 'ua', 'oe', 'gi', 'ban', 'toi', 'khong', 'nhe', 'co', 'khong']
+        vietnamese_keywords = ['la', 'ai', 'bao', 'nao', 'dau', 'cua', 'gi', 'ban', 'toi', 'minh', 'khong', 'nhe', 'co', 'thi', 'thoi', 'the'] # 'ue', 'oi', 'oe', 'ua'
         
         question_lower = question.lower()
         # Check for Vietnamese-specific characters
